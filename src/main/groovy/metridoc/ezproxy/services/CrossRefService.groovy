b74f83aba67cf0a88f46fe020f55d3b320a3acbd
Tommy Barker <mingus.karate@gmail.com>
2013-10-15 11:01:50 -0400

    move CrossRefResponse into separate file

--- src/main/groovy/metridoc/ezproxy/services/CrossRefService.groovy
+++ src/main/groovy/metridoc/ezproxy/services/CrossRefService.groovy
@@ -142,27 +142,3 @@ class CrossRefService extends DefaultTool {
     }
 }
 
-class CrossRefResponse {
-    boolean loginFailure = false
-    boolean malformedDoi = false
-    boolean unresolved = false
-    String status
-    String doi
-    String articleTitle
-    String journalTitle
-    String givenName
-    String surName
-    String volume
-    String issue
-    String firstPage
-    String lastPage
-    Integer printYear
-    Integer electronicYear
-    Integer onlineYear
-    Integer nullYear
-    Integer otherYear
-    String printIssn
-    String electronicIssn
-    String printIsbn
-    String electronicIsbn
-}
